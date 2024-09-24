1ae5657fbba5dfe400ba35970b7b9f2a6921758c
jaxel <axel.jourson@ynov.com>
2024-09-24 15:08:22 +0200

    missing.go

--- Ex2/missing.go
+++ Ex2/missing.go
@@ -11,11 +11,12 @@ func Ft_missing(nums []int) int {
 	for _, num := range nums {
 		actual += num
 	}
+	fmt.Println(expected - actual)
 	return expected - actual
 }
 
 func main() {
-	fmt.Println(Ft_missing([]int{3, 1, 2}))                   // Résultat : 0
-	fmt.Println(Ft_missing([]int{0, 1}))                      // Résultat : 2
-	fmt.Println(Ft_missing([]int{9, 6, 4, 2, 3, 5, 7, 0, 1})) // Résultat : 8
+	Ft_missing([]int{3, 1, 2})                   // Résultat : 0
+	Ft_missing([]int{0, 1})                      // Résultat : 2
+	Ft_missing([]int{9, 6, 4, 2, 3, 5, 7, 0, 1}) // Résultat : 8
 }
