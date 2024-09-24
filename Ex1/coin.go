5536429a58c9d91a5267ee1c5ff9cecbdec3230a
jaxel <axel.jourson@ynov.com>
2024-09-24 15:10:05 +0200

    coin.go

--- Ex1/coin.go
+++ Ex1/coin.go
@@ -24,11 +24,12 @@ func Ft_coin(coins []int, amount int) int {
 		return -1
 	}
 
+	fmt.Println(difference[amount])
 	return difference[amount]
 }
 
 func main() {
-	fmt.Println(Ft_coin([]int{1, 2, 5}, 11)) // resultat : 3 car (11 == 5 + 5 + 1)
-	fmt.Println(Ft_coin([]int{2}, 3))        // resultat : -1
-	fmt.Println(Ft_coin([]int{1}, 0))        // resultat : 0
+	Ft_coin([]int{1, 2, 5}, 11) // resultat : 3 car (11 == 5 + 5 + 1)
+	Ft_coin([]int{2}, 3)        // resultat : -1
+	Ft_coin([]int{1}, 0)        // resultat : 0
 }
